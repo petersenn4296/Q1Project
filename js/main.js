27fad8f5a539e78916a99cd74904b1a403a45329
Peter S <petersenn4296@gmail.com>
2018-05-04 10:11:43 -0600

    create repo and get files into GitHub for devolpment

--- js/main.js
+++ js/main.js
@@ -1,24 +1,55 @@
-$(document).ready(function() {
+ $(document).ready(function() {
+   $(".beginner-card").hide();
+   $(".intermediate-card").hide();
+   $(".advanced-card").hide();
+
+   $(".beginner").click(function() {
+      $(".intermediate-card").hide();
+      $(".advanced-card").hide();
+      $(".beginner-card").fadeIn(750);
 
 
-  //   var unique = require('uniq');
-  // const WolframAlphaAPI = require('wolfram-alpha-api');
-  // const waApi = WolframAlphaAPI('LU5VY9-W54XP5TG4W');
-  // waApi.getFull('sin x').then(console.log).catch(console.error);
-  // waApi.getFull('Inverted Seal Pose').then(console.log).catch(console.error);
+   });
 
 
-  $("#show-pose").click(function() {
-    console.log("clicked it");
 
-    // $.get('https://api.wolframalpha.com/v1/simple?appid=LU5VY9-W54XP5TG4W&i=Inverted+Seal+Pose').then(function(result) {
-    //   console.log("here she blows");
-    // });
-    $.ajax({
-      url: `https://g-wolframaplha.herokuapp.com/?t=the+matrix&y=&plot=short&r=json`,
-      success: function(result) {
-        console.log(result);
-      }
-    });
-  });
-});
+   $(".intermediate").click(function() {
+      $(".beginner-card").hide();
+      $(".advanced-card").hide();
+      $(".intermediate-card").fadeIn(750);
+   });
+
+
+
+   $(".advanced").click(function() {
+      $(".beginner-card").hide();
+      $(".intermediate-card").hide();
+      $(".advanced-card").fadeIn(750);
+   });
+// let url = 'http://g-wolframaplha.herokuapp.com/v2/query?appid=LU5VY9-W54XP5TG4W&input=Inverted+Seal+Pose';
+//
+//   $("#show-pose").click(function() {
+//     console.log("clicked it");
+//
+//
+//
+//
+//     $.ajax({
+//       url: url,
+//       success: function(result) {
+//         console.log(result);
+//       }
+//     });
+//   });
+// });
+
+
+//
+// $.ajax({
+//    url : url,
+// }).always(function(){
+//    $(".image").attr("src", url);
+//
+//
+// });
+ });
