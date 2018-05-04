90f6710b9c2162ed255871399f1287ab53a562bd
Peter S <petersenn4296@gmail.com>
2018-05-04 16:02:33 -0600

    added projcet requirements doc as well as wireframe links

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
  $(document).ready(function() {
-   $(".beginner-card").hide();
-   $(".intermediate-card").hide();
-   $(".advanced-card").hide();
+
+
+// This code pulls up the 8 poses based on the difficulty selection
+$(".beginner-card").hide();
+$(".intermediate-card").hide();
+$(".advanced-card").hide();
 
    $(".beginner").click(function() {
       $(".intermediate-card").hide();
       $(".advanced-card").hide();
       $(".beginner-card").fadeIn(750);
-
-
    });
-
-
 
    $(".intermediate").click(function() {
       $(".beginner-card").hide();
@@ -19,37 +18,50 @@
       $(".intermediate-card").fadeIn(750);
    });
 
-
-
    $(".advanced").click(function() {
       $(".beginner-card").hide();
       $(".intermediate-card").hide();
       $(".advanced-card").fadeIn(750);
    });
-// let url = 'http://g-wolframaplha.herokuapp.com/v2/query?appid=LU5VY9-W54XP5TG4W&input=Inverted+Seal+Pose';
-//
-//   $("#show-pose").click(function() {
-//     console.log("clicked it");
-//
-//
-//
-//
-//     $.ajax({
-//       url: url,
-//       success: function(result) {
-//         console.log(result);
-//       }
-//     });
-//   });
-// });
-
-
-//
-// $.ajax({
-//    url : url,
-// }).always(function(){
-//    $(".image").attr("src", url);
-//
-//
-// });
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// All of this code is to pull data from Wolfram API:
+
+
+ // let url = 'http://g-wolframaplha.herokuapp.com/v2/query?appid=LU5VY9-W54XP5TG4W&input=Inverted+Seal+Pose';
+ //
+ //   $(".buttons").click(function() {
+ //     console.log("clicked it");
+ //
+ //
+ //
+ //
+ //     $.ajax({
+ //       url: url,
+ //       success: function(result) {
+ //         console.log(result);
+ //       }
+ //     });
+ //   });
+
+ // Pulls Wolfram Simple Data as image and makes it appear on the screen
+
+ // $.ajax({
+ //    url : url,
+ // }).always(function(){
+ //    $(".image").attr("src", url);
+ //
+ //
+ // });
