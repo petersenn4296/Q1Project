4388c7d32964007f9f1eccc1f9a597d0c247ad70
Peter S <petersenn4296@gmail.com>
2018-05-07 14:16:30 -0600

    added info to about us page, added login modal, worked on some layout

---
+++ README.txt
@@ -0,0 +1,33 @@
+Welcome to Yogi-Posi-Tracki!!!
+
+This is an app to track your progress on a large number of yoga poses
+and see the progress you are making. When you select a pose you will have
+the choice to see the pose info or add it to your progress. The info will
+provide you with how to get into it, the muscles and joints that benefit
+from the pose as well as the intensity level. If you add it to your
+progress it will add cards to your progress page. The progress Page
+will put the pose you chose in an editable card where you will tell it
+the time you spent practicing the pose and other comments about your
+progress with the pose. Enjoy!
+
+
+
+Storytelling plan:
+
+Trello Story Telling: https://trello.com/b/DCXk2lQI/pose-a-week
+
+
+
+
+
+Wireframes:
+
+
+Landing Page : https://wireframe.cc/uEq0tM
+Click on poses in landing page: https://wireframe.cc/UQ691S
+
+Progress Page : https://wireframe.cc/n5oUiF
+
+Pose info page: https://wireframe.cc/GFfgots
+
+Contact Us page : https://wireframe.cc/s1T9aD
--- js/main.js
+++ js/main.js
@@ -23,8 +23,87 @@ $(".advanced-card").hide();
       $(".intermediate-card").hide();
       $(".advanced-card").fadeIn(750);
    });
- });
+// Opens and closes the log in modal
+var modal = $('#myModal');
+var button = $('#butt');
+var close = $('.close');
+button.click(function () {
+  myModal.style.display = "block";
+});
+close.click(function () {
+  myModal.style.display = "none";
+});
 
+
+// Stores username and password to local storage
+
+
+var un = $(".username");
+var pw = $(".password");
+// Stores UN and PW on click of register
+$(".register").click(function store() {
+    localStorage.setItem('username', un.val());
+    localStorage.setItem('password', pw.val());
+    alert("You have created an account");
+})
+// Checks UN and PW on click of login
+$(".login").click(function check() {
+  console.log("Clicked");
+    // stored data from the register-form
+    var storedName = localStorage.getItem('username');
+    var storedPw = localStorage.getItem('password');
+
+    // check if stored data from register-form is equal to data from login form
+    if(un.val() !== storedName || pw.val() !== storedPw) {
+        alert('Incorrect credentials');
+    }else {
+        alert('You are logged in.');
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let url = 'http://g-wolframaplha.herokuapp.com/v1/simple?appid=LU5VY9-W54XP5TG4W&input=Extended+Triangle+Pose';
+   $(".poses").click(function() {
+     console.log("clicked it");
+
+
+         $.ajax({
+            url : url,
+         }).always(function(){
+            $(".wolf").attr("src", url);
+          });
+
+});
+
+
+
+
+
+ });
+///////////////////////////////////////////////////////////////////
 
 
 
@@ -44,24 +123,21 @@ $(".advanced-card").hide();
  //
  //   $(".buttons").click(function() {
  //     console.log("clicked it");
- //
- //
- //
- //
- //     $.ajax({
- //       url: url,
- //       success: function(result) {
- //         console.log(result);
- //       }
- //     });
- //   });
-
- // Pulls Wolfram Simple Data as image and makes it appear on the screen
-
- // $.ajax({
+ //$.ajax({
  //    url : url,
  // }).always(function(){
  //    $(".image").attr("src", url);
- //
- //
  // });
+ // // $.ajax({
+  //   url: url,
+  //   success: function(result) {
+  //     console.log(result);
+  //     $(".wolf").attr("src", url);
+  //   }
+  // });
+ //
+ //
+
+ //   });
+
+ // Pulls Wolfram Simple Data as image and makes it appear on the screen
